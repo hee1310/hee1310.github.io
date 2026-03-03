f9f4f2cd9db41c9124315710bb2e90068a8ef10f
hee1310 <121546886+hee1310@users.noreply.github.com>
2026-03-03 16:25:33 +0000

    Deploy Hexo blog via Codespaces

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1772554923028";const preCache = ["/images/taichi.png","/images/common/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1772555129653";const preCache = ["/images/taichi.png","/images/common/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
